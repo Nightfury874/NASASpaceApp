3b49a236e23ab2189a26d21103f7bde624b7c14b
Nihar Jyoti Basisth <62636996+Nightfury874@users.noreply.github.com>
2020-10-04 20:45:47 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# NASASpaceApp
-NASA Space App Challenge
+# PRAKRITI
+A Flutter application | ML kit | API Intigration
+
+<img src="images/backg.jpeg" height="500">
+<img src="images/11.jpeg" height="500">
+<img src="images/12.jpeg" height="500">
+<img src="images/13.jpeg" height="500">
+<img src="images/14.jpeg" height="500">
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'splashScreen.dart';
+
+void main() => runApp(MaterialApp(
+  home: SplashScreen(),
+));
+
+
